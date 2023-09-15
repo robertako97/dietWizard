04e9bac3c07b68a500e748f5d343c6ff473a76c7
didierthomasm <didierthomas.m@gmail.com>
2023-09-14 18:44:30 -0600

    fix on server

--- server.js
+++ server.js
@@ -1,30 +1,76 @@
+'use strict'
+// Dependencies
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
+// const morgan = require('morgan');
 const routes = require('./controllers');
 
-const { sequelize } = require('./config/connection');
+require('dotenv').config();
+
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+// Set up Express app
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sess = {
+const hbs = exphbs.create({ defaultLayout: 'main' });
+
+
+// Session
+app.use(session({
     secret: 'Super secret secret',
-    cookie: {},
+    cookie: {
+        maxAge: 300000,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+    } /*{ secure: true }*/,
     resave: false,
     saveUninitialized: true,
-    store: new SequelizeStore({
+    store: new SequelizeStore( {
         db: sequelize
     })
-};
+}));
 
-app.use(session(sess));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(morgan('combined'));
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+// Global error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+
+    if (process.env.NODE_ENV === 'development') {
+        res.status(500).json({
+            message: err.message,
+            stack: err.stack
+        });
+    } else {
+        res.status(500).json({
+            message: 'Internal Server Error'
+        });
+    }
 });
+
+// Sync the server with sequelize and then start the server to begin listening
+const init = async () => {
+    try {
+        await sequelize.sync({ force: false });
+        app.listen(PORT, () => {
+            console.log(`App listening on http://localhost:${PORT}!`);
+        });
+    } catch (error) {
+        console.error('Error syncing the database:', error);
+    }
+};
+
+init();
