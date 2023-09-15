280ff5ebb0c918a664e6c63cc6f6d1a3322ba202
robertako97 <roberto.diazgmx@gmail.com>
2023-09-14 17:59:41 -0700

    models - need to clean

--- models/index.js
+++ models/index.js
@@ -1,43 +1,24 @@
-'use strict';
+const foodPreference = require('./foodPreference');
+const meals = require('./meals'); // Corrected import name
+const userDiet = require('./userDiet');
+const userInfo = require('./userInfo');
+const userLogin = require('./userLogin');
+const userPlan = require('./userPlan');
+const ingredients = require('./ingredients');
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
+foodPreference.belongsTo(userInfo, { foreignKey: 'username_id', onDelete: 'CASCADE'});
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+userInfo.hasOne(userLogin, { foreignKey: 'user_id', onDelete: 'CASCADE' });
+userLogin.belongsTo(userInfo, { foreignKey: 'user_id', onDelete: 'CASCADE' });
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+userInfo.hasOne(userPlan, { foreignKey: 'user_id', onDelete:'CASCADE'});
 
-module.exports = db;
+userDiet.hasMany(meals, { foreignKey: 'diet_id', onDelete: 'CASCADE'});
+
+ingredients.hasMany(meals, { foreignKey: 'ingredients_id'});
+
+
+
+module.exports = { foodPreference, ingredients, meals, userDiet, userInfo, userLogin, userPlan };
