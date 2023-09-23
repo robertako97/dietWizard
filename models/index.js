3b41c2f60525bbabdb2a4c3e9fc59e0380e69ca3
didierthomasm <didierthomas.m@gmail.com>
2023-09-23 12:16:43 -0600

    Renamed userDiet.js to diet.js, added Diet model and renamed index.js to diet_meals.js.

--- models/index.js
+++ models/index.js
@@ -1,24 +1,74 @@
-const foodPreference = require('./foodPreference');
-const meals = require('./meals'); // Corrected import name
-const userDiet = require('./userDiet');
-const User = require('./User');
-const userPlan = require('./userPlan');
-const ingredients = require('./ingredients');
+const Preference = require('./preference');
+const Meals = require('./meals');
+const Diet = require('./diet');
+const User = require('./user');
+const Plan = require('./plan');
+const Ingredients = require('./ingredients');
+const Diet_meals = require('./diet_meals');
+const Individual = require('./individual');
 
+// Setup relationships
+User.hasOne(Individual, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
 
+Individual.belongsTo(User, {
+  foreignKey: 'user_id'
+});
 
-foodPreference.belongsTo(User, { foreignKey: 'username_id', onDelete: 'CASCADE'});
+Individual.hasMany(Preference, {
+  foreignKey: 'individual_id',
+  onDelete: 'CASCADE'
+});
 
+Individual.hasMany(Plan, {
+  foreignKey: 'individual_id',
+  onDelete: 'CASCADE'
+});
 
-User.hasOne(userPlan, { foreignKey: 'user_id', onDelete:'CASCADE'});
+Preference.belongsTo(Individual, {
+  foreignKey: 'individual_id'
+});
 
-userDiet.hasMany(meals, { foreignKey: 'diet_id', onDelete: 'CASCADE'});
+Plan.belongsTo(Individual, {
+  foreignKey: 'individual_id'
+});
 
-userPlan.hasMany(userDiet, { foreignKey: 'plan_id', onDelete: 'CASCADE'});
+Plan.hasMany(Diet, {
+  foreignKey: 'plan_id',
+  onDelete: 'CASCADE'
+});
 
-ingredients.hasMany(meals, { foreignKey: 'ingredients_id'});
+Diet.belongsTo(Plan, {
+  foreignKey: 'plan_id'
+});
 
+Diet.hasMany(Diet_meals, {
+  foreignKey: 'diet_id',
+  onDelete: 'CASCADE'
+});
 
-module.exports = { foodPreference, ingredients, meals, userDiet, User, userPlan };
+Diet_meals.belongsTo(Diet, {
+  foreignKey: 'diet_id'
+});
 
+Diet_meals.belongsTo(Meals, {
+  foreignKey: 'meal_id'
+});
 
+Meals.hasMany(Diet_meals, {
+  foreignKey: 'meal_id',
+  onDelete: 'CASCADE'
+});
+
+Diet_meals.hasMany(Ingredients, {
+  foreignKey: 'diet_meals_id',
+  onDelete: 'CASCADE'
+});
+
+Ingredients.belongsTo(Diet_meals, {
+  foreignKey: 'diet_meals_id'
+});
+
+module.exports = { Preference, Meals, Ingredients, Diet, User, Plan, Diet_meals, Individual };
