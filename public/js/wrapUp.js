7ed6b9ed46f13fdd7c74fa654218eb6109b99599
didierthomasm <didierthomas.m@gmail.com>
2023-09-24 16:03:45 -0600

    Update wrapUp.js and profile.handlebars files: Replace document location to '/profile' on successful wrap up response and modify the profile section in profile.handlebars.

--- public/js/wrapUp.js
+++ public/js/wrapUp.js
@@ -31,7 +31,7 @@ const wrapUpFormHandler = async (event) => {
 
     if (response.ok) {
       console.log('wrapUp response: ', response)
-      document.location.replace('/');
+      document.location.replace('/profile');
     } else {
       alert('Wrap Up failed, verify credentials and try again.');
     }
